622d89e2a1bf979703fee5caa099738717e11c2c
Rahul Kejriwal <Rahul.Kejriwal@microsoft.com>
2021-09-10 08:41:18 +0530

    wip

--- agent.py
+++ agent.py
@@ -1,3 +1,18 @@
+"""
+TODO
+    - rewards
+    - generation of new world
+        - aggregation of actions (market)
+
+"""
+
+import collections
+
+"""
+    Modelling Actors in our simulation
+        (Serves as a wrapper for the RI algo)
+"""
+
 class GovernmentAgent:
     pass
 
@@ -10,17 +25,31 @@ class Agent:
     def plan(self, reward, world):
         return [] # List of actions: buy/sell/create
 
+
+"""
+    Modelling Agent's Actions 
+"""
+
 class Action:
-    pass
+    def __init__(self, agent, resource, qty):
+        self.agent = agent
+        self.resource = resource
+        self.qty = qty
 
 class CreateAction(Action):
-    def __init__(self, resource, qty):
-        pass
+    def __init__(self, agent, resource, qty):
+        super(TransactAction, self).__init__(self, agent, resource, qty)
 
 # negative qty is selling for agent
 class TransactAction(Action):
-    def __init__(self, resource, qty, price):
-        pass
+    def __init__(self, agent, resource, qty, price):
+        super(TransactAction, self).__init__(self, agent, resource, qty)
+        self.price = price
+
+
+"""
+    Modelling the Environment
+"""
 
 class Resource:
     initializationCost = (0, 0) # Number of turns, amount of money
@@ -43,7 +72,61 @@ class ResourceGraph:
         return self.resourceArray
 
 class Market:
-    {}
+    
+    def __init__(self, resourceGraph):
+        self.lastPrices = {resource : None for resource in resourceGraph.resourceArray()}
+
+    # Group actions by resource
+    @staticmethod
+    def __getResourceWiseTransactions(actionPlan):
+        resourceWiseActions = collections.defaultdict(list)
+        for agent in actionPlan:
+            for action in actionPlan[agent]:
+                if not isinstance(action, CreateAction):
+                    resourceWiseActions[action.resource] += [action]
+        return resourceWiseActions
+
+    # Matches order wrt priority
+    @staticmethod
+    def __processResourceExchange(resource, actions):
+        buyActions = filter(lambda x: x.qty > 0, actions)
+        sellActions = filter(lambda x: x.qty < 0, actions)
+
+        orderedBuyActions = sorted(buyActions, key=lambda x: -x.price)
+        orderedSellActions = sorted(sellActions, key=lambda x: x.price)
+
+        # Cant we achieve better matching in case best buyPrice is much higher than best sellPrice? should be able to fill more orders
+        # Example: (assume 1qty oreders)
+        #           BuyPrices  - 1 2 3 4 5
+        #           SellPrices -       4 5 6 7 8
+        # If processing in priority order, matched buy-sell orders: (5,4)
+        # If we do something unfair, matched buy-sell orders: (4,4), (5,5)
+        # 
+        # Also, for price-deltas, we'll assume buyers are dumb and hand over the difference as bonus to sellers
+        executionPlan = []
+        while orderedBuyActions[0].price > orderedSellActions[0].price:
+            bestBuy, bestSell = orderedBuyActions[0], orderedSellActions[0]
+            transQty = min(bestBuy.qty, bestSell.qty)
+            executionPlan += [TransactAction(bestBuy.agent,  resource, transQty, bestBuy.price)]
+            executionPlan += [TransactAction(bestSell.agent, resource, transQty, bestBuy.price)]
+            
+            def updateActionQ(Q, qty):
+                if Q[0].qty == qty: Q.pop(0)
+                else:               Q[0].qty -= transQty
+            updateActionQ(orderedBuyActions, transQty)
+            updateActionQ(orderedSellActions, transQty)
+
+        return executionPlan
+
+    # Returns an executionPlan of approved actions
+    def simulateExchange(self, actionPlan):
+        resourceWiseActions = Market.__getResourceWiseTransactions(actionPlan)
+
+        executionPlan = []
+        for resource, actions in resourceWiseActions.items():
+            executionPlan += Market.__processResourceExchange(resource, actions)
+        return executionPlan
+
 
 class World:
     agentInfo = {}
@@ -57,8 +140,15 @@ class World:
         for agent in agents:
             self.agentInfo[agent]['money'] = worldInitializationConfig['agentMoneyGenerator'](agent)
             self.agentInfo[agent]['resources'] = worldInitializationConfig['agentResourceGenerator'](agent, resourceGraph)
+            self.agentInfo[agent]['appetite'] = worldInitializationConfig['agentAppetiteGenerator'](agent, resourceGraph)
             self.agentInfo[agent]['capabilities'] = set()
         self.resourceGraph = resourceGraph
+
+
+"""
+    Implementing the actual Simulator 
+        (Runs the interactions between the agents & environment)
+"""
 
 class GameConfig:
     pass
@@ -75,12 +165,11 @@ class Simulator:
         self.world = World(worldInitializationConfig, self.agents, resourceGraph)
 
     def runSimulationStep(self):
-        completePlan = {}
+        proposedPlan = []
         for agent in self.agents:
-            agentPlan = agent.plan(self.agentRewards[agent], self.world.agentInfo[agent], self.world.market) # TODO: Define agentRewards
-            completePlan[agent] = agentPlan
+            proposedPlan += agent.plan(self.agentRewards[agent], self.world.agentInfo[agent], self.world.market) # TODO: Define agentRewards
 
-        newWorld = self.generateNewWorld(completePlan)
+        newWorld = self.generateNewWorld(proposedPlan)
         self.world = newWorld
         self.turn += 1
 
